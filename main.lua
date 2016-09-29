89c06a75fc68dcb3381efef66b950e15c0e95ded
KyomaHooin <richard_bruna@nm.cz>
2016-09-29 20:46:48 +0200

    HTTP code

--- main.lua
+++ main.lua
@@ -1,31 +1,23 @@
 -- MAIN
 --GPIO
-
-gpio.mode(4,gpio.OUTPUT)
-on=0
-tmr.alarm(0,2000,1,function()
-if on==0 then
-  on=1
-  gpio.write(4,gpio.LOW)
-else
-  on=0
-  gpio.write(4,gpio.HIGH)
-end
-end)
+--gpio.mode(4,gpio.OUTPUT)
+--on=0
+--tmr.alarm(0,2000,1,function()
+--if on==0 then
+--  on=1
+--  gpio.write(4,gpio.LOW)
+--else
+--  on=0
+--  gpio.write(4,gpio.HIGH)
+--end
+--end)
 
 -- HTTP SOCKET SERVER
---uart.write(0,"Setting HTTP server..")
---sv = net.createServer(net.TCP, 30)
--- server listens on 80, if data received, print data to console and send "hello world" back to caller
---sv:listen(80, function(c)
---  c:on("receive", function(c, pl)
---    print(pl)
---  end)
---  c:send("<html>hello world</html>")
---end)
-
---sv = net.createServer(net.TCP, 30)
---sv:listen(80, function(sock)
---  sock:on("receive", function(sock) sock:send("It work's!") end )
---end)
---print("Done.")
+uart.write(0,"Setting HTTP server..")
+srv = net.createServer(net.TCP, 30)-- TCP server instance
+srv:listen(80, function(sock) -- socket
+	sock:on("connection",function(sock)
+		sock:send("HTTP/1.0 200 OK\r\nServer: NodeMCU on ESP8266\r\nContent-Type: text/html\r\n\r\n<html><head></head><body><b>Hello world!</b></body></html>")
+	end)
+end)
+print("Done.")
