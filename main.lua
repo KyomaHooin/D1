919b80851b4c382c192156a63ca0b156e8de6d1d
KyomaHooin <richard_bruna@nm.cz>
2016-09-29 20:58:36 +0200

    Typo.

--- main.lua
+++ main.lua
@@ -3,8 +3,8 @@ gpio.mode(4,gpio.OUTPUT)
 on=0
 tmr.alarm(0,2000,1,function()
 	if on==0 then
- 		on=1
-		gpio.write(4,gpio.LOW)
+		on=1
+		gpio.write(4,gpio.LOW) -- 10k pull-up resistor..
 	else
 		on=0
 		gpio.write(4,gpio.HIGH)
